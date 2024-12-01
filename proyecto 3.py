bb5a2bae0eb29efcdc550d8403d0cc6bfa266ffa
NicolayB <ne.barrera@uniandes.edu.co>
2024-12-01 10:10:35 -0500

    Importacion de datos

---
+++ proyecto 3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Importación de datos
+data = pd.read_csv("C:/Users/Nicolay Barrera/OneDrive - Universidad de los andes/Documentos/Nicolay/Universidad/IIND/analitica computacional/proyectos/proyecto 3/db final saber11.csv")
+print(data.head())
